250ced710ee99dd74e8f7b96010817a470643211
MUKILTEO\1721537 <1721537@mukilteo.wednet.edu>
2024-10-10 10:22:29 -0700

    Slight correction to the sturm

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -19,30 +19,43 @@ namespace Text_Adventure
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Color meanings: gray = narrator, white = thoughts, blue = officers, red = for user");
-
-            System.Threading.Thread.Sleep(3000);
+            Character character = new()
+            {
+                name = "Kyle",
+                stats = new()
+                {
+                    health = 20,
+                    defense = 5,
+                    damage = 5,
+                }
+            };
+            colorRed();
+            Console.WriteLine("Instructions: gray = narrator, white = thoughts, blue = officers, red = for user, green = talking\n" +
+                "Press 'space' to skip text, but whenever it says 'scene' you can't skip");
+            
+            Console.ReadKey();
 
             string shirtColor; //the color of the shirt is strictly for fun
             int shirtNum; //will decide the code needed to open the cabin door
 
+            colorGray();
             Console.WriteLine("Running through the forest has been a challenging task.\n" +
                 "The dense leaves and sticks of bushes keep getting in the way of the objective ahead");
 
-            System.Threading.Thread.Sleep(4000); //makes console wait before running further code
+            Console.ReadKey();
 
-            Console.ForegroundColor = ConsoleColor.Blue; //blue is the color of other officers
-            Console.WriteLine("Officer 'name', we send backup, but it'll take some time for them to reach you.\n" +
-                "If possible, provide the description of the suspect");
+            colorBlue(); //blue is the color of other officers
+            Console.WriteLine("'Officer " + character.name + ", we send backup, but it'll take some time for them to reach you.\n'" +
+                "'If possible, provide the description of the suspect'");
 
-            System.Threading.Thread.Sleep(3500);
+            Thread.Sleep(200);
 
-            Console.ForegroundColor = ConsoleColor.White; //white is internal dialog
+            colorWhite(); //white is internal dialog
             Console.WriteLine("He is barely visible, but I can make out his shirt... I think");
 
-            System.Threading.Thread.Sleep(3500);
+            Console.ReadKey();
 
-            Console.ForegroundColor = ConsoleColor.Red; //red is the direction to the user
+            colorRed(); //red is the direction to the user
             Console.WriteLine("Choose the color of the shirt. Type EXACTLY what is written in the options\n" +
                 "red | green | light blue");
             shirtColor = Console.ReadLine();
@@ -51,30 +64,94 @@ namespace Text_Adventure
                 "27 | 5 | 88");
             shirtNum = Convert.ToInt32(Console.ReadLine());
 
-            System.Threading.Thread.Sleep(1000);
+            Console.ReadKey();
 
-            Console.ForegroundColor = ConsoleColor.Green; //green is the color of the main character
+            //start scene
+            colorGray();
+            Console.WriteLine("SCENE");
+
+            colorGreen(); //green is the color of the main character
             Console.WriteLine("The shirt is " + shirtColor + " and I think it has a number on it.\n" +
                 "It's..." + shirtNum + "!");
 
-            System.Threading.Thread.Sleep(3000);
+            Thread.Sleep(3500);
 
-            Console.ForegroundColor = ConsoleColor.Blue;
+            colorBlue();
             Console.WriteLine("Roger that!");
 
-            System.Threading.Thread.Sleep(1000);
+            Thread.Sleep(1000);
 
-            Console.ForegroundColor = ConsoleColor.White;
+            colorWhite();
             Console.WriteLine("Okay, now hopefully we'll catch this guy");
 
-            System.Threading.Thread.Sleep(2000);
+            Thread.Sleep(2500);
 
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine("Everything was going well. Backup, 'name' is on his tail, and the forest is fairly small.\n " +
+            colorGray();
+            Console.WriteLine("Everything was going well. Backup, " + character.name + " is on his tail, and the forest is fairly small.\n" +
                 "Nowhere to hide really.\n" +
-                "Until something weird started happening, something that 'name' only just realized.\n" +
+                "Until something weird started happening, something that " + character.name + " only just realized.\n" +
                 "The forest went quiet. There is nothing but his gasping breaths.");
 
+            Thread.Sleep(11000);
+
+            colorBlue();
+            Console.WriteLine(character.name + " to base, what is going on I can't hear you?");
+
+            Thread.Sleep(2200);
+
+            colorGray();
+            Console.WriteLine("But all that came through was a final beep, contact was lost.");
+            Console.Beep(800, 300);
+
+            //end the scene
+
+            Console.ReadKey();
+
+            colorGreen();
+            Console.WriteLine("Just great, awesome! I lost the suspect and I don't have contact with the base!");
+
+            Console.ReadKey();
+
+            colorWhite();
+            Console.WriteLine("But a choice had to be made, preferably now.");
+            
+            colorRed();
+            Console.WriteLine("Where should you go? Type the letter of choice you make(capitalize)\n " +
+                "A) back to base | B) search for suspect | C) wander around aimlessly | D) yell");
+            char choice = Console.ReadKey().KeyChar;
+            colorGray();
+
+            if (choice == 'A')
+            {
+                Console.WriteLine("\nYou went back to try and leave the forest. There is no point in the mission with no contact to base");
+            }
+
+            else if (choice == 'B')
+            {
+                Console.WriteLine("\nYou decide to go forward deeper into the forest, suspect is on the lose and you won't give up that easily");
+            }
+
+            else if (choice == 'C')
+            {
+                Console.WriteLine("\nYou decide to just go on a stroll, not much can be done about the situation");
+            }
+
+            else if (choice == 'D')
+            {
+                Console.WriteLine("\nYou scream into the shadows of the forest, hoping to achieve the unknown\n" +
+                    "But then, you see something jump out at you!\n" +
+                    "As soon as it's on top of you, you feel agonizing pain erupt in your stomach.\n" +
+                    "Then, it's darkness and cold.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            else
+            {
+                Console.WriteLine("\nWhat are you doing?");
+            }
+
+            Console.WriteLine("doesn't work");
 
             Console.ReadKey();
         }
@@ -82,6 +159,22 @@ namespace Text_Adventure
         static void colorWhite()
         {
             Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void colorGray()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        static void colorRed()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        static void colorGreen()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        static void colorBlue()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
         }
     }
 }
