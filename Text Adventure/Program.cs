b1d246db55c0a7d9167688d52a89ff70e295c78c
ElinaMalice <1721537@mukilteo.wednet.edu>
2024-10-13 19:07:53 -0700

    Created the first branch off, finished D, worked on  A. You get to the cabin after getting lost and the beeping sound starts.

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using Microsoft.VisualBasic;
+using System.Drawing;
 
 namespace Text_Adventure
 {
@@ -8,6 +9,7 @@ namespace Text_Adventure
         {
             public string name;
             public Stats stats;
+            public string[] items;
         }
 
         struct Stats
@@ -16,6 +18,16 @@ namespace Text_Adventure
             public int defense;
             public int damage;
         }
+
+        enum Objects
+        {
+            Gun,
+            Knife,
+            Water,
+            Vest,
+            Paper,
+            Files
+        };
 
         static void Main(string[] args)
         {
@@ -27,14 +39,18 @@ namespace Text_Adventure
                     health = 20,
                     defense = 5,
                     damage = 5,
-                }
+
+                },
+                items = new string[] {Objects.Gun.ToString(), "", "", "", "", ""}
             };
+
+
             colorRed();
             Console.WriteLine("Instructions: gray = narrator, white = thoughts, blue = officers, red = for user, green = talking\n" +
-                "Press 'space' to skip text, but whenever it says 'scene' you can't skip");
+                "Press any key to skip text, but whenever it says 'scene' you can't skip");
             
-            Console.ReadKey();
-
+            Console.ReadKey(true);
+            /*
             string shirtColor; //the color of the shirt is strictly for fun
             int shirtNum; //will decide the code needed to open the cabin door
 
@@ -42,7 +58,7 @@ namespace Text_Adventure
             Console.WriteLine("Running through the forest has been a challenging task.\n" +
                 "The dense leaves and sticks of bushes keep getting in the way of the objective ahead");
 
-            Console.ReadKey();
+            Console.ReadKey(true);
 
             colorBlue(); //blue is the color of other officers
             Console.WriteLine("'Officer " + character.name + ", we send backup, but it'll take some time for them to reach you.\n'" +
@@ -53,18 +69,60 @@ namespace Text_Adventure
             colorWhite(); //white is internal dialog
             Console.WriteLine("He is barely visible, but I can make out his shirt... I think");
 
-            Console.ReadKey();
+            Console.ReadKey(true);
 
             colorRed(); //red is the direction to the user
             Console.WriteLine("Choose the color of the shirt. Type EXACTLY what is written in the options\n" +
                 "red | green | light blue");
             shirtColor = Console.ReadLine();
 
-            Console.WriteLine("Now choose a number.\n" +
-                "27 | 5 | 88");
-            shirtNum = Convert.ToInt32(Console.ReadLine());
+            if (shirtColor == "red")
+            {
 
-            Console.ReadKey();
+            }
+            else if (shirtColor == "red")
+            {
+
+            }
+            else if (shirtColor == "light blue")
+            {
+
+            }
+            else
+            {
+                shirtColor = "yellow";
+            }
+
+                Console.WriteLine("Now choose a number.\n" +
+                "27 | 5 | 88");
+            try 
+            { 
+                shirtNum = Convert.ToInt32(Console.ReadLine()); 
+            }
+            catch
+            {
+                shirtNum = 64;
+            }
+
+            if (shirtNum == 27)
+            {
+
+            }
+            else if (shirtNum == 5)
+            {
+
+            }
+            else if (shirtNum == 88)
+            {
+
+            }
+            else
+            {
+                shirtNum = 64;
+            }
+
+
+            Console.ReadKey(true);
 
             //start scene
             colorGray();
@@ -104,26 +162,151 @@ namespace Text_Adventure
             Console.Beep(800, 300);
 
             //end the scene
+            Thread.Sleep(3000);
+            Console.Clear();
 
-            Console.ReadKey();
+            colorRed();
+            Console.WriteLine("Press any key to continue");
+
+            Console.ReadKey(true);
 
             colorGreen();
-            Console.WriteLine("Just great, awesome! I lost the suspect and I don't have contact with the base!");
+            Console.WriteLine("Just great, awesome! I lost the suspect, and I don't have contact with the base!");
 
-            Console.ReadKey();
-
+            Console.ReadKey(true);
+            
             colorWhite();
             Console.WriteLine("But a choice had to be made, preferably now.");
-            
+            */
             colorRed();
             Console.WriteLine("Where should you go? Type the letter of choice you make(capitalize)\n " +
                 "A) back to base | B) search for suspect | C) wander around aimlessly | D) yell");
-            char choice = Console.ReadKey().KeyChar;
             colorGray();
+
+
+            // asks the user which of the 4 things will they do
+            // back to base, search for suspect, wander around, yell
+            whereToGo();
+
+            Console.ReadKey(true);
+        }
+
+        static void colorWhite()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        //narrator color
+        static void colorGray()
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+        static void colorRed()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        static void colorGreen()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        static void colorBlue()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+        }
+
+        static void whereToGo()
+        {
+            char choice = Console.ReadKey(true).KeyChar;
 
             if (choice == 'A')
             {
+                Character character = new()
+                {
+                    name = "Kyle",
+                    stats = new()
+                    {
+                        health = 20,
+                        defense = 5,
+                        damage = 5,
+
+                    },
+                    items = new string[] { Objects.Gun.ToString(), "", "", "", "", "" }
+                };
+
                 Console.WriteLine("\nYou went back to try and leave the forest. There is no point in the mission with no contact to base");
+                Console.ReadKey(true);
+
+                Console.WriteLine("As you walk through the thickets, you notice the sun.");
+                Console.ReadKey(true);
+
+                colorWhite();
+                Console.WriteLine("That thing is way too close to the horizon, I should have already been out of the forest");
+                Console.ReadKey(true);
+
+                colorGray();
+                Console.WriteLine("But it only got darker, and the realization had dawned on you.\n" +
+                    "You are lost in a forest, where the killer you were chasing is wandering around, with no contact to base.");
+                Console.ReadKey(true);
+
+                Console.WriteLine("And so you walk...");
+                int number = 0;
+                do
+                {
+                    Console.WriteLine("and walk...");
+                    number++;
+                } while (number <= 2);
+
+                Console.WriteLine("And keep walking around in hopes that you find something, all while keeping your hand close to the " + character.items[0]);
+                Console.ReadKey(true);
+
+                Console.WriteLine("Eventully, after what felt like hours of aimless walking, with the sun almost reaching horizon, you stuble upon a cabin.\n" +
+                    "It looks almost ancient, and sends shivers down your spine when you look at it, but it's more than the plain forest surrounding you");
+                Console.ReadKey(true);
+
+                Console.WriteLine("You start moving closer to it, and then you hear a peculiar sound");
+
+                bool insideCabin = false;
+
+                for (int boo = 7; insideCabin == false;)
+                {
+                    Console.Beep(500, 900);
+                    Console.Beep(500, 400);
+                    Console.Beep(500, 900);
+                    Console.Beep(500, 400);
+                    Thread.Sleep(900);
+
+                    Console.Beep(500, 900);
+                    Console.Beep(500, 900);
+                    Console.Beep(500, 900);
+                    Thread.Sleep(900);
+
+                    Console.Beep(500, 400);
+                    Console.Beep(500, 900);
+                    Console.Beep(500, 900);
+                    Thread.Sleep(900);
+
+                    Console.Beep(500, 400);
+                    Console.Beep(500, 900);
+                    Thread.Sleep(900);
+
+                    Console.Beep(500, 400);
+                    Console.Beep(500, 900);
+                    Console.Beep(500, 400);
+                    Thread.Sleep(900);
+
+                    Console.Beep(500, 900);
+                    Console.Beep(500, 400);
+                    Console.Beep(500, 400);
+                    Thread.Sleep(4000);
+
+                }
+
+
+
+                Console.ReadKey();
+
+                insideCabin = true;
+
             }
 
             else if (choice == 'B')
@@ -141,40 +324,18 @@ namespace Text_Adventure
                 Console.WriteLine("\nYou scream into the shadows of the forest, hoping to achieve the unknown\n" +
                     "But then, you see something jump out at you!\n" +
                     "As soon as it's on top of you, you feel agonizing pain erupt in your stomach.\n" +
-                    "Then, it's darkness and cold.");
-                Console.ReadKey();
+                    "Then, it turns dark and cold...");
+                colorGray();
+                Console.WriteLine("You died");
+                Console.ReadKey(true);
                 Environment.Exit(0);
             }
 
             else
             {
-                Console.WriteLine("\nWhat are you doing?");
+                Console.WriteLine("\nThat's not an option");
+                whereToGo();
             }
-
-            Console.WriteLine("doesn't work");
-
-            Console.ReadKey();
-        }
-
-        static void colorWhite()
-        {
-            Console.ForegroundColor = ConsoleColor.White;
-        }
-        static void colorGray()
-        {
-            Console.ForegroundColor = ConsoleColor.Gray;
-        }
-        static void colorRed()
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-        }
-        static void colorGreen()
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-        }
-        static void colorBlue()
-        {
-            Console.ForegroundColor = ConsoleColor.Blue;
         }
     }
 }
